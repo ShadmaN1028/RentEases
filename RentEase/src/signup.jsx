dff8978f4fc7c7cfb62fa2cd55c1fdd219037fb6
ShadmaN1028 <shadmansaleheen0928@gmail.com>
2024-06-03 22:58:43 +0600

    asdfghjk

--- RentEase/src/signup.jsx
+++ RentEase/src/signup.jsx
@@ -17,7 +17,7 @@ function SignUp() {
           <form action="" className="justify-left flex mt-[40px] ml-[100px] ">
             <input
               type="text"
-              className="h-[35px] w-[325px]  rounded-[20px] px-3 bg-background placeholder-black font-normal focus:ring-1 focus:ring-inset focus:ring-indigo-600 outline-none hover:ring-1 hover:ring-indigo-600 hover:ring-inset shadow-md hover:shadow-md hover:shadow-slate-900/25"
+              className="h-[35px] w-[325px]  rounded-[20px] px-4 bg-background placeholder-black font-normal focus:ring-1 focus:ring-inset focus:ring-indigo-600 outline-none hover:ring-1 hover:ring-indigo-600 hover:ring-inset shadow-md hover:shadow-md hover:shadow-slate-900/25"
               placeholder="Name"
             />
           </form>
