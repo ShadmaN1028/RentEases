b5c0306cb94a0507632bfd8cebb86dc05e7b358f
ShadmaN1028 <shadmansaleheen0928@gmail.com>
2024-06-04 22:39:52 +0600

    leftside fix

--- RentEase/src/signin.jsx
+++ RentEase/src/signin.jsx
@@ -6,13 +6,13 @@ function SignIn() {
   return (
     <>
       <div className="flex">
-        <div className="bg-background h-screen w-[44%]">
-          <div className=" text-right mt-[150px] mr-[120px]">
-          <p className="text-7xl font-bold">RentEase!!!</p>
-          <div className="ml-[200px] mr-[120px] mt-10 w-[280px]">
-            <p className="text-xl text-left">Find Your Home,</p>
-            <p className="text-xl text-right">Simplify Your Life</p>
-          </div>
+        <div className="bg-background items-end h-screen w-[44%]">
+          <div className=" text-center flex flex-col justify-center items-center mt-[150px]">
+            <p className=" text-7xl font-bold">RentEase!!!</p>
+            <div className="mt-10 w-[45%]">
+              <p className="text-xl text-left">Find Your Home,</p>
+              <p className="text-xl text-right">Simplify Your Life</p>
+            </div>
           </div>
         </div>
         <div className="bg-white h-screen w-[56%]">
@@ -20,17 +20,28 @@ function SignIn() {
             <p className=" text-4xl font-bold">Continue Your Journey</p>
             <p className=" text-4xl font-bold">With RentEase!!!</p>
           </div>
-          <form action="" className="justify-left flex flex-col space-y-[20px] mt-[60px] ml-[200px] w-[325px] ">
-
+          <form
+            action=""
+            className="justify-left flex flex-col space-y-[20px] mt-[60px] ml-[200px] w-[325px] "
+          >
             <input type="Email" className="inputf" placeholder="Email" />
 
             <input type="password" className="inputf" placeholder="Password" />
-            <input type="button" value="Sign In" className=" h-[35px] w-[325px]  rounded-[20px] bg-button px-3 text-white shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline hover:bg-button/85" />
+            <input
+              type="button"
+              value="Sign In"
+              className=" h-[35px] w-[325px]  rounded-[20px] bg-button px-3 text-white shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline hover:bg-button/85"
+            />
             <div className="flex flex-row  justify-between">
-            <p className="text-md font-semibold py-1">Don't Have An Account ?</p>
-            <Link to={'/signup'} className=" h-[35px] w-[115px] py-1 rounded-[20px] bg-black text-center text-white shadow-md cursor-pointer focus-visible:outline hover:bg-black/80 hover:shadow-md hover:shadow-slate-900/25">
-            Sign Up
-            </Link>
+              <p className="text-md font-semibold py-1">
+                Don't Have An Account ?
+              </p>
+              <Link
+                to={"/signup"}
+                className=" h-[35px] w-[115px] py-1 rounded-[20px] bg-black text-center text-white shadow-md cursor-pointer focus-visible:outline hover:bg-black/80 hover:shadow-md hover:shadow-slate-900/25"
+              >
+                Sign Up
+              </Link>
             </div>
           </form>
         </div>
--- RentEase/src/signup.jsx
+++ RentEase/src/signup.jsx
@@ -6,13 +6,13 @@ function SignUp() {
   return (
     <>
       <div className="flex">
-        <div className="bg-background h-screen w-[44%]">
-          <div className=" text-right mt-[150px] mr-[120px]">
-          <p className="text-7xl font-bold">RentEase!!!</p>
-          <div className="ml-[200px] mr-[120px] mt-10 w-[280px]">
-            <p className="text-xl text-left">Find Your Home,</p>
-            <p className="text-xl text-right">Simplify Your Life</p>
-          </div>
+        <div className="bg-background items-end h-screen w-[44%]">
+          <div className=" text-center flex flex-col justify-center items-center mt-[150px]">
+            <p className=" text-7xl font-bold">RentEase!!!</p>
+            <div className="mt-10 w-[45%]">
+              <p className="text-xl text-left">Find Your Home,</p>
+              <p className="text-xl text-right">Simplify Your Life</p>
+            </div>
           </div>
         </div>
         <div className="bg-white h-screen w-[56%] ">
@@ -20,7 +20,10 @@ function SignUp() {
             <p className=" text-4xl font-bold">Begin Your Journey</p>
             <p className=" text-4xl font-bold">With RentEase!!!</p>
           </div>
-          <form action="" className="justify-left flex flex-col space-y-[20px] mt-[60px] ml-[200px] w-[325px] ">
+          <form
+            action=""
+            className="justify-left flex flex-col space-y-[20px] mt-[60px] ml-[200px] w-[325px] "
+          >
             <input type="text" className="inputf" placeholder="Name" />
 
             <input type="Email" className="inputf" placeholder="Email" />
@@ -47,14 +50,27 @@ function SignUp() {
                 className="h-[35px] w-[100px]  rounded-[20px] px-3 bg-background placeholder-black font-normal focus:ring-1 focus:ring-inset focus:ring-indigo-600 outline-none hover:ring-1 hover:ring-indigo-600 hover:ring-inset shadow-md hover:shadow-md hover:shadow-slate-900/25"
                 placeholder="OTP"
               />
-              <input type="button" value="Get OTP" className="h-[35px] w-[90px] rounded-[20px] bg-button px-3 text-white shadow-md cursor-pointer focus-visible:outline hover:bg-button/85 hover:shadow-md hover:shadow-slate-900/25" />
-            </div >
-            <input type="button" value="Sign Up" className=" h-[35px] w-[325px]  rounded-[20px] bg-black px-3 text-white shadow-md cursor-pointer focus-visible:outline hover:bg-black/80 hover:shadow-md hover:shadow-slate-900/25" />
+              <input
+                type="button"
+                value="Get OTP"
+                className="h-[35px] w-[90px] rounded-[20px] bg-button px-3 text-white shadow-md cursor-pointer focus-visible:outline hover:bg-button/85 hover:shadow-md hover:shadow-slate-900/25"
+              />
+            </div>
+            <input
+              type="button"
+              value="Sign Up"
+              className=" h-[35px] w-[325px]  rounded-[20px] bg-black px-3 text-white shadow-md cursor-pointer focus-visible:outline hover:bg-black/80 hover:shadow-md hover:shadow-slate-900/25"
+            />
             <div className="flex flex-row  justify-between">
-            <p className="text-md font-semibold py-1">Already Have An Account ?</p>
-            <Link to={'/signin'} className=" h-[35px] w-[115px] py-1 rounded-[20px] bg-black text-center text-white cursor-pointer focus-visible:outline hover:bg-black/80 shadow-md hover:shadow-md hover:shadow-slate-900/25">
-            Sign In
-            </Link>
+              <p className="text-md font-semibold py-1">
+                Already Have An Account ?
+              </p>
+              <Link
+                to={"/signin"}
+                className=" h-[35px] w-[115px] py-1 rounded-[20px] bg-black text-center text-white cursor-pointer focus-visible:outline hover:bg-black/80 shadow-md hover:shadow-md hover:shadow-slate-900/25"
+              >
+                Sign In
+              </Link>
             </div>
           </form>
         </div>
