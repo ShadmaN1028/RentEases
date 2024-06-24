9381de7a2ec9aed61c071f1c011172b1fc5f52e6
Raian Ruku <raianruku21@gmail.com>
2024-06-24 16:15:21 +0600

    added prettier plugin for tailwind

--- RentEase/src/flatsinfo.jsx
+++ RentEase/src/flatsinfo.jsx
@@ -1,70 +1,70 @@
-import React from 'react'
-import img from './assets/owner.png'
+import React from "react";
+import img from "./assets/owner.png";
 
 function FlatsInfo() {
   return (
     <>
-    <div className="flex flex-col min-h-screen min-w-[1024px]">
-      <div className='flex flex-col justify-center items-center'>
-        <img src= {img} className="h-[300px] w-[300px] rounded-[50%]" alt="" />
-            <h1 className="text-3xl font-bold mb-6">Miss Chanandler Bong</h1>
+      <div className="flex min-h-screen min-w-[1024px] flex-col">
+        <div className="flex flex-col items-center justify-center">
+          <img src={img} className="h-[300px] w-[300px] rounded-[50%]" alt="" />
+          <h1 className="mb-6 text-3xl font-bold">Miss Chanandler Bong</h1>
+        </div>
+        <div className="flex flex-row justify-evenly">
+          <div className="ml-[50px] flex h-auto w-[400px] cursor-pointer flex-col items-center space-y-[20px] rounded-[20px] bg-background py-[30px] shadow-md hover:shadow-md hover:shadow-slate-900/25 focus-visible:outline">
+            <p className="text-center text-xl font-bold">House name</p>
+            <div className="flex flex-row justify-evenly">
+              <p>House#191/1</p>
+              <p className="pl-[10%]">Flat#A</p>
+            </div>
+            <div className="mb-[20px] flex flex-col items-center justify-center space-y-[20px]">
+              <p>Tenant: Joey Tribbiani</p>
+              <p>Contact: 01900000000</p>
+              <p>Payment Status: Unpaid</p>
+            </div>
+            <input
+              type="button"
+              value="Details"
+              className="h-[40px] w-[120px] cursor-pointer rounded-[20px] bg-button px-3 text-white shadow-md hover:bg-button/85 hover:shadow-md hover:shadow-slate-900/25 focus-visible:outline"
+            />
+          </div>
+          <div className="ml-[50px] flex h-auto w-[400px] cursor-pointer flex-col items-center space-y-[20px] rounded-[20px] bg-background py-[30px] shadow-md hover:shadow-md hover:shadow-slate-900/25 focus-visible:outline">
+            <p className="text-center text-xl font-bold">House name</p>
+            <div className="flex flex-row justify-evenly">
+              <p>House#191/1</p>
+              <p className="pl-[10%]">Flat#B</p>
+            </div>
+            <div className="mb-[20px] flex flex-col items-center justify-center space-y-[20px]">
+              <p>Tenant: Monica Geller</p>
+              <p>Contact: 01900000000</p>
+              <p>Payment Status: Unpaid</p>
+            </div>
+            <input
+              type="button"
+              value="Details"
+              className="h-[40px] w-[120px] cursor-pointer rounded-[20px] bg-button px-3 text-white shadow-md hover:bg-button/85 hover:shadow-md hover:shadow-slate-900/25 focus-visible:outline"
+            />
+          </div>
+          <div className="ml-[50px] flex h-auto w-[400px] cursor-pointer flex-col items-center space-y-[20px] rounded-[20px] bg-background py-[30px] shadow-md hover:shadow-md hover:shadow-slate-900/25 focus-visible:outline">
+            <p className="text-center text-xl font-bold">House name</p>
+            <div className="flex flex-row justify-evenly">
+              <p>House#191/1</p>
+              <p className="pl-[10%]">Flat#C</p>
+            </div>
+            <div className="mb-[20px] flex flex-col items-center justify-center space-y-[20px]">
+              <p>Tenant: Ross Geller</p>
+              <p>Contact: 01900000000</p>
+              <p>Payment Status: Unpaid</p>
+            </div>
+            <input
+              type="button"
+              value="Details"
+              className="h-[40px] w-[120px] cursor-pointer rounded-[20px] bg-button px-3 text-white shadow-md hover:bg-button/85 hover:shadow-md hover:shadow-slate-900/25 focus-visible:outline"
+            />
+          </div>
+        </div>
       </div>
-      <div className='flex flex-row justify-evenly'>
-        <div className='h-auto w-[400px] ml-[50px] py-[30px] bg-background space-y-[20px]  rounded-[20px] shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline flex flex-col items-center'>
-        <p className='font-bold text-center text-xl'>House name</p>
-        <div className='flex flex-row justify-evenly'>
-          <p>House#191/1</p>
-          <p className='pl-[10%]'>Flat#A</p>
-        </div>
-        <div className='flex flex-col space-y-[20px] justify-center items-center mb-[20px]'>
-          <p>Tenant: Joey Tribbiani</p>
-          <p>Contact: 01900000000</p>
-          <p>Payment Status: Unpaid</p>
-        </div>
-        <input
-              type="button"
-              value="Details"
-              className=" h-[40px] w-[120px] rounded-[20px] bg-button px-3 text-white shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline hover:bg-button/85"
-            />
-        </div>
-        <div className='h-auto w-[400px] ml-[50px] py-[30px] bg-background space-y-[20px]  rounded-[20px] shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline flex flex-col items-center'>
-        <p className='font-bold text-center text-xl'>House name</p>
-        <div className='flex flex-row justify-evenly'>
-          <p>House#191/1</p>
-          <p className='pl-[10%]'>Flat#B</p>
-        </div>
-        <div className='flex flex-col space-y-[20px] justify-center items-center mb-[20px]'>
-          <p>Tenant: Monica Geller</p>
-          <p>Contact: 01900000000</p>
-          <p>Payment Status: Unpaid</p>
-        </div>
-        <input
-              type="button"
-              value="Details"
-              className=" h-[40px] w-[120px] rounded-[20px] bg-button px-3 text-white shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline hover:bg-button/85"
-            />
-        </div>
-        <div className='h-auto w-[400px] ml-[50px] py-[30px] bg-background space-y-[20px]  rounded-[20px] shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline flex flex-col items-center'>
-        <p className='font-bold text-center text-xl'>House name</p>
-        <div className='flex flex-row justify-evenly'>
-          <p>House#191/1</p>
-          <p className='pl-[10%]'>Flat#C</p>
-        </div>
-        <div className='flex flex-col space-y-[20px] justify-center items-center mb-[20px]'>
-          <p>Tenant: Ross Geller</p>
-          <p>Contact: 01900000000</p>
-          <p>Payment Status: Unpaid</p>
-        </div>
-        <input
-              type="button"
-              value="Details"
-              className=" h-[40px] w-[120px] rounded-[20px] bg-button px-3 text-white shadow-md hover:shadow-md hover:shadow-slate-900/25 cursor-pointer focus-visible:outline hover:bg-button/85"
-            />
-        </div>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default FlatsInfo
+export default FlatsInfo;
--- RentEase/tailwind.config.js
+++ RentEase/tailwind.config.js
@@ -1,18 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  mode : 'jit',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   theme: {
     extend: {
       colors: {
-        'background': '#d2d2d2',
-        'button' : '#49c41e'
+        background: "#d2d2d2",
+        button: "#49c41e",
       },
     },
   },
-  plugins: [],
-}
-
+  plugins: ["prettier-plugin-tailwindcss"],
+};
